bf854168ddb83c8684b3e06e8e2cab59c759651e
zeli000000 <xiaxin698@gmail.com>
2023-03-19 14:44:35 +0800

    gshop

---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,138 @@
+<template>
+  <header class="header">
+          <slot name="left"></slot>
+          <span class="header_title">
+            <span class="header_title_text ellipsis">{{title}}</span>
+          </span>
+          <slot name="right"></slot>
+         
+        </header>
+</template>
+
+<script>
+export default {
+    props:{
+     title:String
+    }
+
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+            .header
+            background-color #02a774
+            position fixed
+            z-index 100
+            left 0
+            top 0
+            width 100%
+            height 45px
+            .header_search
+              position absolute
+              left 15px
+              top 50%
+              transform translateY(-50%)
+              width 10%
+              height 50%
+              .icon-sousuo
+                font-size 25px
+                color #fff
+            .header_title
+              position absolute
+              top 50%
+              left 50%
+              transform translate(-50%, -50%)
+              width 50%
+              color #fff
+              text-align center
+              .header_title_text
+                font-size 20px
+                color #fff
+                display block
+            .header_login
+              font-size 14px
+              color #fff
+              position absolute
+              right 15px
+              top 50%
+              transform translateY(-50%)
+              .header_login_text
+                color #fff
+          width 100%
+          .msite_header
+            background-color #02a774
+            position fixed
+            z-index 100
+            left 0
+            top 0
+            width 100%
+            height 45px
+            .header_search
+              position absolute
+              left 15px
+              top 50%
+              transform translateY(-50%)
+              width 10%
+              height 50%
+              .icon-sousuo
+                font-size 25px
+                color #fff
+            .header_title
+              position absolute
+              top 50%
+              left 50%
+              transform translate(-50%, -50%)
+              width 50%
+              color #fff
+              text-align center
+              .header_title_text
+                font-size 20px
+                color #fff
+                display block
+            .header_login
+              font-size 14px
+              color #fff
+              position absolute
+              right 15px
+              top 50%
+              transform translateY(-50%)
+              .header_login_text
+                color #fff
+          .msite_nav
+            bottom-border-1px(#e4e4e4)
+            margin-top 45px
+            height 200px
+            background #fff
+            .swiper-container
+              width 100%
+              height 100%
+              .swiper-wrapper
+                width 100%
+                height 100%
+                .swiper-slide
+                  display flex
+                  justify-content center
+                  align-items flex-start
+                  flex-wrap wrap
+                  .link_to_food
+                    width 25%
+                    .food_container
+                      display block
+                      width 100%
+                      text-align center
+                      padding-bottom 10px
+                      font-size 0
+                      img
+                        display inline-block
+                        width 50px
+                        height 50px
+                    span
+                      display block
+                      width 100%
+                      text-align center
+                      font-size 13px
+                      color #666
+              .swiper-pagination
+                >span.swiper-pagination-bullet-active
+                  background #02a774
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    2123123123
+  </main>
+</template>
